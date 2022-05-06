fae66d18453e1f9daa28c0df8dafb64940f011a0
Mouhammed-Alaqeel <muhammed_alaqeel@hotmail.com>
2022-05-06 20:00:28 +0300

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LogInComponent } from './Components/log-in/log-in.component';
 import { ButtonComponent } from './Components/button/button.component';
-import { InputComponent } from './Components/input/input.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
  
@@ -14,7 +13,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     AppComponent,
     LogInComponent,
     ButtonComponent,
-    InputComponent,
+    
   ],
   imports: [
     BrowserModule,
